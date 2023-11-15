854b58a58fdb65f859742421943df94e2181b673
Nabilsenko <96882497+Nabilsenko@users.noreply.github.com>
2023-11-15 13:58:48 +0200

    refactor(data): replace local data with TMDB API call for keyword search method

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import seriesRouter from './routes/series';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
--- src/routes/series.ts
+++ src/routes/series.ts
@@ -1,20 +1,41 @@
 import express, { Router, Request, Response } from 'express';
-import { seriesData } from '../data/seriesData';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
+const TMDB_API_BASE_URL = 'https://api.themoviedb.org/3';
 
 const seriesRouter: Router = express.Router();
 
-seriesRouter.get('/search', (req: Request, res: Response) => {
+seriesRouter.get('/search', async (req: Request, res: Response) => {
+  const url = `${TMDB_API_BASE_URL}/search/tv`;
+
   const { keyword } = req.query;
 
   if (!keyword) {
     return res.status(400).json({ error: 'Keyword is required.' });
   }
+  console.log('*********** TMDB_API_KEY:', TMDB_API_KEY);
 
-  const matchingSeries = seriesData.filter((serie) =>
-    serie.name.toLowerCase().includes(keyword.toString().toLowerCase()),
-  );
+  try {
+    const tmdbResponse = await axios.get(url, {
+      params: {
+        query: keyword,
+        page: 1,
+      },
+      headers: {
+        Authorization: `Bearer ${TMDB_API_KEY}`,
+      },
+    });
 
-  res.json(matchingSeries);
+    const matchingSeries = tmdbResponse.data.results;
+
+    res.json(matchingSeries);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch data from TMDB API.' });
+  }
 });
 
 export default seriesRouter;
